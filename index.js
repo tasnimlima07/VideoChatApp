847c211933ba00413213b664481cdec8b55e37b8
tasnimlima07 <tasnimlima07@gmail.com>
2024-04-02 00:50:20 +0600

    'index.js'

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send('server is running.');
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
