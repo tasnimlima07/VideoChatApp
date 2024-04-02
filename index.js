d5b514e4338a31c68b4749de6d94bbf796aaf804
tasnimlima07 <tasnimlima07@gmail.com>
2024-04-02 12:08:32 +0600

    'index.js'

--- index.js
+++ index.js
@@ -20,4 +20,22 @@ app.get("/", (req, res) => {
     res.send('server is running.');
 });
 
+
+io.on('connection', socket => {
+    socket.emit('me', socket.io);
+    // When someone attempts to join the room
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended');
+    });
+
+    socket.on("calluser", ({ userToCall, singnalData, from, name}) => {
+        io.to(userToCall).emit("calluser", {singnal: singnalData, from, name});
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callacepted", data.singnal);
+    });
+        
+});
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
